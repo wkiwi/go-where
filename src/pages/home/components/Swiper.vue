fe5a656a5d96f4a989791476f3eb4f206986a124
wkiwi <15619986615@163.com>
2018-08-30 14:38:45 +0800

    change

---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption" ref="mySwiper" >
+      <!-- slides -->
+      <swiper-slide v-for="item of swiperList" :key="item.id">
+        <img class="swiper-img" :src="item.url" >
+      </swiper-slide>
+      <!-- Optional controls -->
+      <div class="swiper-pagination"  slot="pagination"></div>
+      <!-- <div class="swiper-button-prev" slot="button-prev"></div>
+      <div class="swiper-button-next" slot="button-next"></div> 
+      <div class="swiper-scrollbar"   slot="scrollbar"></div>-->
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+   name: 'HomeSwiper',
+   data () {
+       return {
+        swiperOption: {
+          pagination:'.swiper-pagination',
+          loop:true
+        },
+        swiperList: [{
+            id: "001",
+            url: 'https://source.qunarzz.com/site/images/wap/home/recommend/iphoneplus/dujia_shuqi_banner_20180710.jpg'
+        },{
+            id: "002",
+            url: 'https://source.qunarzz.com/site/images/wap/home/recommend/iphoneplus/anquan_20180716.png'
+        }]
+      }
+   } 
+}
+</script>
+
+<style lang="stylus" scoped>
+  .wrapper >>> .swiper-pagination-bullet-active
+    background:#fff
+  .wrapper
+    overflow:hidden
+    width:100%
+    height:0
+    padding-bottom:25%
+    background:#eee
+    .swiper-img
+      width:100%
+</style>
