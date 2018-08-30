9ba5102ae7123bae1a70cf045183ca7f9f38f357
wkiwi <15619986615@163.com>
2018-08-30 16:53:08 +0800

    add icons

--- src/pages/home/components/Icons.vue
+++ src/pages/home/components/Icons.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="icons">
         <swiper>
-            <swiper-slide v-for="page of pages" :key="page">
+            <swiper-slide v-for="(page,index) of pages" :key="index">
                 <div class="icon" v-for="item of page" :key="item.id">
                     <div class="icon-img">
                         <img class="icon-img-content" :src="item.imgUrl">
