b75711dfa9585c46e42a3cb7090ceae848670cbd
wkiwi <15619986615@163.com>
2018-08-31 14:48:42 +0800

    city-components

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -2,8 +2,8 @@
     <div>
         <city-header></city-header>
         <city-search></city-search>
-        <city-list :cities="cities" :hot="hotCities"></city-list>
-        <city-alphabet :cities="cities"></city-alphabet>   
+        <city-list :letter="letter" :cities="cities" :hot="hotCities"></city-list>
+        <city-alphabet :cities="cities" @change="handleLetterChange"></city-alphabet>   
     </div>
 </template>
 
@@ -24,7 +24,8 @@ export default {
     data () {
         return {
             cities:{},
-            hotCities:[]
+            hotCities:[],
+            letter:""
         }
     },
     methods:{
@@ -39,6 +40,10 @@ export default {
                 this.cities=data.cities
                 this.hotCities=data.hotCities
             }
+        },
+        handleLetterChange:function(letter){//监听子组件传过来的信息
+            console.log(letter)
+            this.letter=letter
         }
     },
     mounted () {
--- src/pages/city/components/Alphabet.vue
+++ src/pages/city/components/Alphabet.vue
@@ -1,6 +1,6 @@
 <template>
     <ul class="list">
-        <li class="item" v-for="(item,key) of cities" :key="key">{{key}}</li>
+        <li class="item" v-for="(item,key) of cities" :ref="key" :key="key" @click="handleLetterClick" @touchstart="handleTouchStart" @touchmove="handleTouchMove" @touchend="handleTouchEnd">{{key}}</li>
     </ul>
 </template>
 
@@ -9,6 +9,52 @@ export default {
     name: 'CityAlphabet',
     props:{
         cities:Object
+    },
+    computed:{
+        letters () {
+            const letters=[]
+            for(let i in this.cities){
+                letters.push(i)
+            }
+            return letters
+        }
+    },
+    data(){
+        return {
+            touchStatus:false,
+            startY:0,
+            timer:null
+        }
+    },
+    updated(){
+        this.startY =this.$refs['A'][0].offsetTop
+    },
+    methods:{
+        handleLetterClick:function(e){
+            this.$emit('change',e.target.innerText)
+            console.log(e.target.innerText)
+        },
+        handleTouchStart:function(){
+            this.touchStatus=true
+        },
+        handleTouchMove:function(e){
+            if(this.touchStatus){
+                if(this.timer){
+                    clearTimeout(this.timer)
+                }
+                this.timer=setTimeout(()=>{
+                    //const startY=this.$refs['A'][0].offsetTop
+                    const touchY=e.touches[0].clientY-79
+                    const index=Math.floor((touchY-this.startY)/20)
+                    if(index>=0 && index<this.letters.length){
+                        this.$emit('change',this.letters[index])
+                    }
+                },16)   
+            }
+        },
+        handleTouchEnd:function(){
+            this.touchStatus=false
+        } 
     }
 }
 </script>
